a3567e2c4b56db908f2c6683a55c801880960f4a
Santhosh136 <snthosh602@gmail.com>
2026-01-10 00:13:15 +0530

    Add Withdraw method to Wallet and update tests for balance assertion

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -6,18 +6,29 @@ import (
 
 func TestWallet(t *testing.T) {
 
+	assertBalance := func(t testing.TB, w Wallet, want Bitcoin) {
+		t.Helper()
+		balance := w.Balance()
+		if balance != want {
+			t.Errorf("got %s but want %s", balance, want)
+		}
+	}
+
 	t.Run("Deposit money to the wallet", func(t *testing.T) {
 
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(100))
 
-		balance := wallet.Balance()
 		want := Bitcoin(100)
+		assertBalance(t, wallet, want)
+	})
 
-		if balance != want {
-			t.Errorf("got %s but want %s", balance, want)
-		}
+	t.Run("Withdraw money from the wallet", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(200)}
+		wallet.Withdraw(Bitcoin(100))
 
+		want := Bitcoin(100)
+		assertBalance(t, wallet, want)
 	})
 
 }

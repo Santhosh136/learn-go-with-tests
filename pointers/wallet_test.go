8c121cbe128b665105136c90e40df43ee0892c87
Santhosh136 <snthosh602@gmail.com>
2026-01-09 23:25:12 +0530

    Add Wallet and Bitcoin types with Deposit and Balance methods

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -1,1 +1,23 @@
 package pointers
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+
+	t.Run("Deposit money to the wallet", func(t *testing.T) {
+
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(100))
+
+		balance := wallet.Balance()
+		want := Bitcoin(100)
+
+		if balance != want {
+			t.Errorf("got %s but want %s", balance, want)
+		}
+
+	})
+
+}
